948e17fbbce765a51f3173c69d87419a716bc2ba
personal-prtflio <brooke.construction1@gmail.com>
2026-02-02 13:04:58 -0500

    Create checkpoints.js

---
+++ checkpoints.js
@@ -0,0 +1,30 @@
+import * as THREE from './libs/three.module.js';
+
+export class CheckpointSystem {
+  constructor(scene) {
+    this.scene = scene;
+    this.checkpoints = [];
+    this.current = 0;
+  }
+
+  addCheckpoint(position) {
+    const geometry = new THREE.SphereGeometry(5, 16, 16);
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe:true });
+    const checkpoint = new THREE.Mesh(geometry, material);
+    checkpoint.position.copy(position);
+    this.scene.add(checkpoint);
+    this.checkpoints.push(checkpoint);
+  }
+
+  checkCar(car) {
+    if (this.checkpoints.length === 0) return;
+
+    const cp = this.checkpoints[this.current];
+    const distance = car.position.distanceTo(cp.position);
+    if (distance < 7) { // car reached checkpoint
+      cp.material.color.set(0x00ff00); // mark as completed
+      this.current++;
+      console.log(`Checkpoint ${this.current} reached!`);
+    }
+  }
+}
