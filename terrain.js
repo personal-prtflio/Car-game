2a2bcde6ce7d30e6950ef3f321d8ca757c3f2989
personal-prtflio <brooke.construction1@gmail.com>
2026-02-02 13:58:49 -0500

    Update terrain.js

--- terrain.js
+++ terrain.js
@@ -1,6 +1,6 @@
-import * as THREE from './libs/three.module.js';
-import { GLTFLoader } from './libs/GLTFLoader.js';
-import SimplexNoise from './libs/SimplexNoise.js';
+import * as THREE from '/libs/three.module.js';
+import { GLTFLoader } from '/libs/GLTFLoader.js';
+import SimplexNoise from '/libs/SimplexNoise.js';
 
 export function generateTerrain(scene) {
   const size = 2000;
